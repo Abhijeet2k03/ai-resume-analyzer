955db4867f6dafa4f2cb331bc36d2606ddc3a800
Abhijeet2k03 <abhidkmishra11@gmail.com>
2026-09-07 21:58:51 +0530

    Authentication

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,11 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import {useStore} from "zustand/react";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {OPS} from "pdfjs-dist";
+import dependency = OPS.dependency;
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +29,11 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init()
+  },[init]);
   return (
     <html lang="en">
       <head>
@@ -33,6 +43,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
         <Scripts />
